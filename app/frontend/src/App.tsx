70d206e53ca04821f11a9e5f2f0334095f1c2b1d
celarcon <santicelarcon@gmail.com>
2023-07-31 17:41:47 +0200

    frontend test

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+	return (
+		<div className="App">
+			<h3>Vite React Best Practices Template</h3>
+		</div>
+	);
+}
---
+++ app/frontend/tests/App.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { App } from "../src/App";
+
+test("App component display header", () => {
+	render(<App />);
+
+	const heading = screen.getByText("Vite React Best Practices Template");
+
+	expect(heading).toBeInTheDocument();
+});
